d0b41079c28ea4d11e41052f15813341312efd5f
jlopez48 <143027091+jlopez48@users.noreply.github.com>
2024-04-02 20:36:11 -0400

    added logo to get started page

--- dashboard.php
+++ dashboard.php
@@ -10,10 +10,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-	<link rel="stylesheet" href="dashboard.css">	
+	<link rel="stylesheet" href="dashboard.css">
     <script src="dashboard.js"></script>
 </head>
 <body>
+
+  <div class="logo">
+    <a href="index.html">
+      <img src="logo2.3.png" alt="logo">
+      <span style="color:#01939c; font-size:26px; font-weight:bold; letter-spacing: 1px;margin-left: 20px;"></span>
+    </a>
+  </div>
 	<form action="logout.php" method="post" class="logout-button" onclick="return onLogoutClick(); ">
         <button type="submit" id="logout">Logout</button>
     </form>
