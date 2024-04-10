66350a2816a2bff76d568b6ec28a1edb19e6e56c
jlopez48 <143027091+jlopez48@users.noreply.github.com>
2024-04-09 21:15:29 -0400

    made changes to connect to database

--- submit.php
+++ submit.php
@@ -1,17 +1,6 @@
 <?php
 
-$host = "localhost";
-$user = "";
-$pass = "";
-$dbname = "";
-
-// create connection
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-// check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include("connectDB.php");
 
 // sql to create table
 $users = "CREATE TABLE IF NOT EXISTS users (
